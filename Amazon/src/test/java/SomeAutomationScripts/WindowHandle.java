c73ef421de3823785111edbff6f157f2b1d3686c
Dell <Dell@Alekh>
2023-10-11 14:56:10 +0530

    Some code added

--- Amazon/src/test/java/SomeAutomationScripts/WindowHandle.java
+++ Amazon/src/test/java/SomeAutomationScripts/WindowHandle.java
@@ -3,6 +3,7 @@ package SomeAutomationScripts;
 import java.util.Set;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
@@ -32,7 +33,10 @@ public class WindowHandle extends AppUtils {
 		driver.findElement(By.linkText("Images")).click();
 		driver.close();
 		driver.switchTo().window(windowid);
-		driver.findElement(By.xpath("//button[text()='Launch Broweser 1']")).click();
+//		driver.findElement(By.xpath("//button[text()='Launch Broweser 1']"));
+		
+		driver.navigate().to("https://grotechminds.com/javascript-popup/");
+		driver.switchTo().alert().accept();
 			
 		
 		
